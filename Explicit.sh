af29ff4a5f3690ec10001059e49509ced407d13f
ExplicitCyberTeam <46949775+ExplicitCyberTeam@users.noreply.github.com>
2019-01-23 11:13:56 +0700

    Add files via upload

---
+++ Explicit.sh
@@ -0,0 +1,118 @@
+9#!/system/xbin/bash
+
+#LiatApaAsu
+#AuthorCodingSusah²lutinggalrecode
+#lugamikirapasusahnyacoding
+#dasarscriptkiddie
+#CodeByEzGeneralTz
+
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+###################################################
+# CTRL C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Explicit Cyber Team Was Here ... "
+read enter
+exit
+}
+
+echo $white"—————————————————————————————————————————————————————————————————"
+echo $white"                       WELCOME TO MY TOOLS                       "
+echo $white"—————————————————————————————————————————————————————————————————"
+echo $white"Author : EzGeneralTz"
+echo $white"Team   : Explicit Cyber Team - Insomnia Cyber Team - BLACKHAMMER"
+echo $white"Tipe   : INSTALLERv3"
+echo $white"FB     : Satria Andhika Adi Saputro"
+echo $white"IG     : @andhika907"
+echo $white"Thanks : Legion - ®i212 - Mr.H4RI-X - Cyber_Trex - MNSTR - Rmvs"
+echo $white"   •SunDeath• - Floxite_ - MR.BG467 - Mr.BL4CK_F0R3ST - Killer Crown"
+echo $white"——————————————————————————————————————————————————————————————————"
+echo $white"               We Can Find You But You Can't Find Us.             "
+echo $white"——————————————————————————————————————————————————————————————————"
+date
+echo $white"Apakah Anda Yakin Ingin Menggunakan Tools Explicit Cyber Team?"
+echo $white"1->Ya"
+echo $white"2->Tidak"
+read -p "[ExGeneralTz> " bro;
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+echo $white"Login Admin"
+sleep 2
+echo $white"username:ExGeneralTz"
+sleep 2
+echo $white"password:***********"
+sleep 2
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+echo $white"LOADING..."
+sleep 1
+clear
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/ExplicitCyberTeam/ex
+cd ex
+sh Ex.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+echo $white"Cie Cie Yang Ga Punya Paketan"
+sleep 2
+echo $white"Apa Harus Juga Gua Yang Beliin :v"
+sleep 2
+echo $white"Yaudah Sana Beli Paketan Dulu :v"
+sleep 2
+echo $white"Entar Balik Lagi Yah Kalo Udh Punya Paketan :v"
+sleep 2
+exit
+fi
